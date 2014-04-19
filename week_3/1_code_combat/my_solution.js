4835fec73df94f1e5151407ef029ad48c285bd3e
Katie <kreiner@umich.edu>
2014-04-19 15:16:42 -0400

    code combat solutions and my own level!

--- week_3/1_code_combat/my_solution.js
+++ week_3/1_code_combat/my_solution.js
@@ -4,15 +4,131 @@
 // Note: to make commenting easier, you can highlight the section you want to comment and hold 
 // command + / This will comment the line. 
  
+ //move down x2
+//move right
+//move up x2
+//move right x2
+//move down
+//attack!!!
+
+this.moveDown();
+this.moveDown();
+this.moveRight();
+this.moveUp();
+this.moveUp();
+this.moveRight();
+this.moveRight();
+this.moveDown();
+this.attackNearbyEnemy();
+
+//move up
+//move right
+//get mushroom!
+//move left
+//move up
+//attack!!!
+
+this.moveUp();
+this.moveRight();
+this.moveLeft();
+this.moveUp();
+this.attackNearbyEnemy();
+
+//move right
+//attack!!!
+//move right
+//move up
+//move right
+//attack!!!
+
+this.moveRight();
+this.attackNearbyEnemy();
+this.moveRight();
+this.moveDown();
+this.moveUp();
+this.moveRight();
+this.attackNearbyEnemy();
  
- 
- 
- 
- 
- 
- 
- 
+this.moveRight();
+this.bustDownDoor();
+this.moveRight();
+this.say("Hey there!");
+this.moveLeft();
+this.moveLeft();
+this.say("Attack!");
+this.moveRight();
+this.say("Follow me.");
+this.moveRight();
+this.moveRight();
+this.moveUp();
+this.moveRight();
+this.say("you suck");
+this.say("attack");
+this.attackNearbyEnemy();
+this.attackNearbyEnemy();
+this.say("follow me");
+this.moveRight();
+this.moveRight();
+
+this.moveDown();
+this.moveDown();
+this.say("hey orge!");
+this.moveUp();
+this.moveUp(); 
+
+this.say("hey orge!");
+this.say("you're stupid");
+this.say("i hate you");
+this.say("you smell");
+this.moveDown();
+
+this.moveXY(50, 16);
+this.moveXY(63, 20);
+this.moveXY(70, 10);  
+this.say("I can lure them in here.");
+this.moveXY(53,36)
+this.say("come here")
+this.moveXY(73,10)
+
+this.moveXY(49, 66);
+this.moveXY(60, 63);
+this.moveXY(75, 63);
+this.say("Hail, friends!");
+this.say("follow, friends!");
+moveXY(63,43)
+this.say("attack!");
+
 // Reflection:
 // Write your reflection here.
 
-// I'm finally working off my own fork!!!!
+// I'm finally working off my own fork!!!! 
+// this is referring to an object which is Thorn but in programming sense it would just be an object
+// () is what you are doing to the object
+// im not positive but i know they are needed to execute the code 
+
+// Collaboraters
+// Armen Vartan
+// Katie Reiner
+
+// Parameters
+// this.say("I come in peace");
+// Friendlies: Archer
+// Enemies: Ogre
+// Objective 1: Free archer
+// Objective 2: Trick/lure ogre
+// Objective 3: Kill Ogre
+// Map: http://flockdraw.com/gallery/view/1872977
+// Commands: moveXY(), say(), openDoor(), tipToeXY(), attack()
+this.moveXY(4,5);
+this.attack();
+this.moveXY(2, 5);
+this.openDoor();
+this.say("follow me");
+this.tipToeXY(7,3);
+this.openDoor();
+this.tiptoeXY(1,1);
+this.say("Attack!")
+this.attack();
+this.moveXY(7,0);
+
+//
